14a1207ff159e92ab9d178e44721c57dc88ad7b1
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-10-07 10:18:22 -0600

    feat: :art: Add `StoreProcedures` class

---
+++ DevExpress.Utils/Constants/StoreProcedures/StoreProcedures.cs
@@ -0,0 +1,10 @@
+namespace DevExpress.Utils.Constants.StoreProcedures;
+
+public class StoreProcedures
+{
+    #region Store Procedures
+    public const string AddIncome = "AddIncome";
+    public const string AddExpense = "AddExpense";
+    public const string GenerateBiweeklySummary = "GenerateBiweeklySummary";
+    #endregion
+}
